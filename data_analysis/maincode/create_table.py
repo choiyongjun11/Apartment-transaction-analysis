bb62f42b4a2ad64a2f26855022419f638cf9b2b6
sjniya <ardsnyang@gmail.com>
2025-06-03 23:45:52 +0900

    주석 제거

--- data_analysis/maincode/create_table.py
+++ data_analysis/maincode/create_table.py
@@ -3,8 +3,8 @@ from database.handler import Handler
 
 if __name__ == '__main__':
   #DB Table 추가
-  #Handler().create_AptTransaction()
-  #Handler().create_Infrastructure()
-  #Handler().create_BusUsage()
-  #Handler().create_PopulationStats()
+  Handler().create_AptTransaction()
+  Handler().create_Infrastructure()
+  Handler().create_BusUsage()
+  Handler().create_PopulationStats()
   Handler().create_Score()
